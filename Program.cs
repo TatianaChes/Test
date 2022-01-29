82bd9bab02b0bc96347432e996b44f0ff07480e9
Chesnokova <tatianachesss@yandex.ru>
2022-01-29 10:05:56 +0500

    deleted header

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Header!");
+           
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Way!");
         }
